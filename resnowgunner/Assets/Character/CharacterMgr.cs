2e5ab60590610240d2f575aea1d336291bb640fd
ryadjs <ryadjs@ac143119-a72e-0510-8848-c859b1d1e1a5>
2016-04-11 02:38:40 +0000

    CharacterMgr

---
+++ resnowgunner/Assets/Character/CharacterMgr.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using SimpleJSON;
+
+/*
+0 None
+1 기본 캐릭터
+2 숨겨진 캐릭터 1
+3 숨겨진 캐릭터 2
+4 숨겨진 캐릭터 3
+5 숨겨진 캐릭터 4
+*/
+
+public struct CharacterFactorInfo
+{
+    public eCharacterType CharacterType;
+    public string Name;
+    public string Health;
+    public string DaySpd;
+    public string NightSpd;
+}
+public class CharacterMgr : BaseMgr<CharacterMgr>
+{
+    public string m_temp = string.Empty;
+
+    Dictionary<string, CharacterTemplateData> m_dicTemplateData = new Dictionary<string, CharacterTemplateData>();
+
+    //sjh 배치Character정보 - Server로 보내는 Data로 연계시켜야함.
+    List<string> m_listSelectingCharacter = new List<string>();
+
+    public List<string> SELECTING_CHARACTER
+    {
+        get { return m_listSelectingCharacter; }
+        set { m_listSelectingCharacter = value; }
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        //Min-Goo 2016년 4월 11일 오전 3시 56분 JSON Parsing
+        TextAsset chatex = Resources.Load<TextAsset>("CHARIC_TEMPL");
+
+        if (chatex != null)
+        {
+            JSONClass chajson = JSON.Parse(chatex.text) as JSONClass;
+
+            if (chajson != null)
+            {
+                JSONClass chatemple = chajson["CHARIC_TEMPLATE"] as JSONClass;
+                foreach (KeyValuePair<string, JSONNode> keyValue in chatemple)
+                {
+                    //m_dicTemplateData
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ resnowgunner/Assets/Character/CharacterTemplateData.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public enum eCharacterType
+{
+    CHARACTER_TYPE_NONE,
+    CHARACTER_TYPE_MAIN,
+    CHARACTER_TYPE_ASSIST,
+    CHARACTER_TYPE_FRIEND,
+    CHARACTER_TYPE_ENEMY,
+    CHARACTER_TYPE_COUNT,
+}
+public enum eRaceType
+{
+    RACE_TYPE_NONE,
+    RACE_TYPE_HUMAN,
+    RACE_TYPE_ELF,
+    RACE_TYPE_WORGEN,
+}
+public class CharacterTemplateData
+{
+    eCharacterType m_CharacterType = eCharacterType.CHARACTER_TYPE_NONE;
+    eRaceType m_RaceType = eRaceType.RACE_TYPE_NONE;
+    // m_FactorTable에 Json으로 기본 수치를 받아놓을것 임
+
+    string m_strKey   = string.Empty;
+    string m_strName  = string.Empty;
+
+    int   m_nHealth   = 0;
+    float m_fDaySpd   = 0.0f;
+    float m_fNightSpd = 0.0f;
+
+    int m_nRequiredLv  = 0;
+    bool m_bPurchasing = false;
+
+    string m_strHobby    = string.Empty;
+    int m_nRequiredPrice = 0;
+
+    List<string> m_listExtraClass = new List<string>();
+    List<float> m_flistExtraClassParam = new List<float>();
+    
+    public List<string> LIST_EXTRA_CLASS { get { return m_listExtraClass; } }
+    public List<float> LIST_EXTRA_CLASS_PARAM_F { get { return m_flistExtraClassParam; } }
+
+
+
+    public CharacterTemplateData(string strkey, SimpleJSON.JSONNode nodeData)
+    {
+        m_strKey = strkey;
+
+        //"CHARACTER_TYPE": 1
+        //"NAME": "캐릭터1"
+        m_CharacterType = (eCharacterType)(nodeData["CHARACTER_TYPE"].AsInt);
+        m_strName = nodeData["NAME"];
+        //m_strPrefabName = nodeData["PREFAB_NAME"];
+
+        FindRace(nodeData["RACE"].Value);
+
+        m_nHealth   = nodeData["HEALTH"].AsInt;
+        m_fDaySpd   = nodeData["DAY_SPEED"].AsFloat;
+        m_fNightSpd = nodeData["NIGHT_SPEED"].AsFloat;
+        //PLUS_EXP
+        SimpleJSON.JSONArray arrExtraClass = nodeData["EXTRA"].AsArray;
+        SimpleJSON.JSONArray arrExtraClassParam = nodeData["EXTRA_PARAM"].AsArray;
+
+        m_nRequiredLv    = nodeData["LEVEL"].AsInt;
+        m_bPurchasing    = nodeData["PURCHASING"] == "Y" ? true : false;
+
+        m_strHobby       = nodeData["HOBBY"];
+        m_nRequiredPrice = nodeData["PRICE"].AsInt;
+
+        if (arrExtraClass != null)
+        {
+            for (int i = 0; i < m_listExtraClass.Count; ++i)
+            {
+                m_listExtraClass.Add(arrExtraClass[i]);
+            }
+        }
+
+        if (arrExtraClassParam != null)
+        {
+            for (int i = 0; i < m_flistExtraClassParam.Count; ++i)
+            {
+                m_flistExtraClassParam.Add(arrExtraClassParam[i].AsFloat);
+            }
+        }
+    }
+
+    void FindRace(string RaceType)
+    {
+        
+        switch (RaceType)
+        {
+            case "HUMAN":
+                m_RaceType = eRaceType.RACE_TYPE_HUMAN;
+                break;
+            case "ELF":
+                m_RaceType = eRaceType.RACE_TYPE_ELF;
+                break;
+            case "WORGEN":
+                m_RaceType = eRaceType.RACE_TYPE_WORGEN;
+                break;
+            default:
+                m_RaceType = eRaceType.RACE_TYPE_NONE;
+                break;
+        }
+
+    }
+}
