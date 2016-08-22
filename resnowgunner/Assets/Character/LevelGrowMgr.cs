8dc8a0c983a2e49059432d18896c2d6204f03578
ryadjs <ryadjs@ac143119-a72e-0510-8848-c859b1d1e1a5>
2016-08-22 15:42:59 +0000

    m_dicLevelGrowTable 에 아래의 키에 따른 레벨 테이블 추가 (LEVEL, EXP, DIFF)
    CHARACTER_1
    CHARACTER_2
    CHARACTER_JOHN
    CHARACTER_MARTIN
    CHARACTER_RICHARD
    CHARACTER_ROBERT
    CHARACTER_CATHERINE

--- resnowgunner/Assets/Character/LevelGrowMgr.cs
+++ resnowgunner/Assets/Character/LevelGrowMgr.cs
@@ -16,29 +16,119 @@ public class LevelGrowMgr : BaseMgr<LevelGrowMgr> {
     {
         TextAsset textLevelGrowTemplate = Resources.Load(strFileName) as TextAsset;
 
-
         if (textLevelGrowTemplate != null)
         {
+            // 텍스트 파일을 읽어올 것입니다.
             JSONClass CharacterGrowDataNode = JSON.Parse(textLevelGrowTemplate.text) as JSONClass;
             if (CharacterGrowDataNode != null)
             {
                 // 키에 따라 값을 재할당하고 싶다.
-                JSONArray CharacterExpData_Character_1 = CharacterGrowDataNode["CHARACTER_1"] as JSONArray;
-                
+                JSONArray arrCharacter_1 = CharacterGrowDataNode["CHARACTER_1"] as JSONArray;
+                JSONArray arrCharacter_2 = CharacterGrowDataNode["CHARACTER_2"] as JSONArray;
+                JSONArray arrCharacter_JOHN = CharacterGrowDataNode["CHARACTER_JOHN"] as JSONArray;
+                JSONArray arrCharacter_MARTHIN = CharacterGrowDataNode["CHARACTER_MARTIN"] as JSONArray;
+                JSONArray arrCharacter_RICHARD = CharacterGrowDataNode["CHARACTER_RICHARD"] as JSONArray;
+                JSONArray arrCharacter_ROBERT = CharacterGrowDataNode["CHARACTER_ROBERT"] as JSONArray;
+                JSONArray arrCharacter_CATHERINE = CharacterGrowDataNode["CHARACTER_CATHERINE"] as JSONArray;
 
+                // 기본 캐릭터
+                if (arrCharacter_1 != null)
+                {
+                    List<LevelGrowTemplate> m_list = new List<LevelGrowTemplate>();
+                    for (int i = 0, imax = arrCharacter_1.Count; i < imax; ++i)
+                    {
+                        LevelGrowTemplate _template = new LevelGrowTemplate(arrCharacter_1[i]);
+                        m_list.Add(_template);
+                        Debug.Log("LEVEL = " + _template.LEVEL + ", EXP = " + _template.EXP + ", DIFF = " + _template.DIFF);
+                    }
+                    m_dicLevelGrowTable.Add("CHARACTER_1", m_list);
+                }
+
+                // 여자 아처(여성궁수)
+                if (arrCharacter_2 != null)
+                {
+                    List<LevelGrowTemplate> m_list = new List<LevelGrowTemplate>();
+                    for (int i = 0, imax = arrCharacter_2.Count; i < imax; ++i)
+                    {
+                        LevelGrowTemplate _template = new LevelGrowTemplate(arrCharacter_2[i]);
+                        m_list.Add(_template);
+                        Debug.Log("LEVEL = " + _template.LEVEL + ", EXP = " + _template.EXP + ", DIFF = " + _template.DIFF);
+                    }
+                    m_dicLevelGrowTable.Add("CHARACTER_2", m_list);
+                }
+                // 존 거너
+                if (arrCharacter_JOHN != null)
+                {
+                    List<LevelGrowTemplate> m_list = new List<LevelGrowTemplate>();
+                    for (int i = 0, imax = arrCharacter_JOHN.Count; i < imax; ++i)
+                    {
+                        LevelGrowTemplate _template = new LevelGrowTemplate(arrCharacter_JOHN[i]);
+                        m_list.Add(_template);
+                        Debug.Log("LEVEL = " + _template.LEVEL + ", EXP = " + _template.EXP + ", DIFF = " + _template.DIFF);
+                    }
+                    m_dicLevelGrowTable.Add("CHARACTER_JOHN", m_list);
+                }
+                // 마틴 거너
+                if (arrCharacter_MARTHIN != null)
+                {
+                    List<LevelGrowTemplate> m_list = new List<LevelGrowTemplate>();
+                    for (int i = 0, imax = arrCharacter_MARTHIN.Count; i < imax; ++i)
+                    {
+                        LevelGrowTemplate _template = new LevelGrowTemplate(arrCharacter_MARTHIN[i]);
+                        m_list.Add(_template);
+                        Debug.Log("LEVEL = " + _template.LEVEL + ", EXP = " + _template.EXP + ", DIFF = " + _template.DIFF);
+                    }
+                    m_dicLevelGrowTable.Add("CHARACTER_MARTHIN", m_list);
+                }
+                // 리차드 거너
+                if (arrCharacter_RICHARD != null)
+                {
+                    List<LevelGrowTemplate> m_list = new List<LevelGrowTemplate>();
+                    for (int i = 0, imax = arrCharacter_RICHARD.Count; i < imax; ++i)
+                    {
+                        LevelGrowTemplate _template = new LevelGrowTemplate(arrCharacter_RICHARD[i]);
+                        m_list.Add(_template);
+                        Debug.Log("LEVEL = " + _template.LEVEL + ", EXP = " + _template.EXP + ", DIFF = " + _template.DIFF);
+                    }
+                    m_dicLevelGrowTable.Add("CHARACTER_RICHARD", m_list);
+                }
+                // 로버트 거너
+                if (arrCharacter_ROBERT != null)
+                {
+                    List<LevelGrowTemplate> m_list = new List<LevelGrowTemplate>();
+                    for (int i = 0, imax = arrCharacter_ROBERT.Count; i < imax; ++i)
+                    {
+                        LevelGrowTemplate _template = new LevelGrowTemplate(arrCharacter_ROBERT[i]);
+                        m_list.Add(_template);
+                        Debug.Log("LEVEL = " + _template.LEVEL + ", EXP = " + _template.EXP + ", DIFF = " + _template.DIFF);
+                    }
+                    m_dicLevelGrowTable.Add("CHARACTER_ROBERT", m_list);
+                }
+                // 캐서린 마법사
+                if (arrCharacter_CATHERINE != null)
+                {
+                    List<LevelGrowTemplate> m_list = new List<LevelGrowTemplate>();
+                    for (int i = 0, imax = arrCharacter_CATHERINE.Count; i < imax; ++i)
+                    {
+                        LevelGrowTemplate _template = new LevelGrowTemplate(arrCharacter_CATHERINE[i]);
+                        m_list.Add(_template);
+                        Debug.Log("LEVEL = " + _template.LEVEL + ", EXP = " + _template.EXP + ", DIFF = " + _template.DIFF);
+                    }
+                    m_dicLevelGrowTable.Add("CHARACTER_CATHERINE", m_list);
+                }
+                /* JSONArray 전체 출력
                 foreach (KeyValuePair<string,JSONNode> keyValue in CharacterGrowDataNode)
                 {
+                    Debug.Log(keyValue);
                     List<LevelGrowTemplate> m_list = null;
                     if(m_dicLevelGrowTable.TryGetValue(keyValue.Key,out m_list )==false)
                     {
                         m_list = new List<LevelGrowTemplate>();
-
                         for (int i = 0, imax = keyValue.Value.Count; i < imax; ++i)
                         {
                             LevelGrowTemplate _template = new LevelGrowTemplate(keyValue.Value[i]);
                             m_list.Add(_template);
                         }
-
                         m_dicLevelGrowTable.Add(keyValue.Key, m_list);
                     }
                     else
@@ -46,158 +136,13 @@ public class LevelGrowMgr : BaseMgr<LevelGrowMgr> {
                         for (int i = 0, imax = keyValue.Value.Count; i < imax; ++i)
                         {
                             LevelGrowTemplate _template = new LevelGrowTemplate(keyValue.Value[i]);
-
                             if (m_list.Contains(_template) == false)
                                 m_list.Add(_template);
                         }
                     }
-                    
-
-                }
+                }*/
                 Debug.Log(m_dicLevelGrowTable);
-
-
-
-                //foreach (KeyValuePair<string, JSONNode> keyValue in CharacterExpData_Character_1)
-                //{
-                //    LevelGrowTemplate _Character_1Temp = new LevelGrowTemplate(keyValue.Key);
-                //    List<LevelGrowTemplate> m_list = null;
-                //    if (m_dicLevelGrowTable.ContainsKey(keyValue.Key) == false)
-                //    {
-                //        // m_list == JSONNode 불일치
-                //        m_list = new List<LevelGrowTemplate>();
-                //        //Error1
-                //        /*LevelGrowTemplate levelGrowTemplate = LevelGrowTemplate(keyValue.Key);
-                //        m_list.Add(levelGrowTemplate);
-                //        m_dicLevelGrowTable.Add(keyValue.Key, m_list);*/
-                //    }
-                //    else
-                //    {
-                //        m_list.Add(_Character_1Temp);
-                //    }
-                //    m_list.Add(_Character_1Temp);
-                //}
             }
-            /* 현재 위의 예전의 소스
-            for (int i = 0; i < arrCharacter_1.Count; ++i)
-            {
-                LevelGrowTemplate _Character_1Temp = new LevelGrowTemplate(arrCharacter_1[i]);
-                List<LevelGrowTemplate> m_list = null;
-                LevelGrowTable _Character_1Temp = new LevelGrowTable(arrCharacter_1[i]);
-                if (m_dicLevelGrowTable.ContainsKey(arrCharacter_1) == false)
-                {
-                    m_list = new List<LevelGrowTemplate>();
-                    m_dicLevelGrowTemplate.Add(arrCharacter_1, m_list);
-                }
-                else
-                {
-                    m_list = m_dicLevelGrowTemplate[arrCharacter_1];
-                }
-                m_list.Add(_Character_1Temp);
-            }*/
-
-            /* 여자 아처(여성궁수)
-            for (int i = 0; i < arrCharacter_2.Count; ++i)
-            {
-                LevelGrowTemplate _Character_2Temp = new LevelGrowTemplate(arrCharacter_2[i]);
-                List<LevelGrowTemplate> m_list = null;
-                if (m_dicLevelGrowTemplate.ContainsKey(arrCharacter_2) == false)
-                {
-                    m_list = new List<LevelGrowTemplate>();
-                    m_dicLevelGrowTemplate.Add(arrCharacter_2, m_list);
-                }
-                else
-                {
-                    m_list = m_dicLevelGrowTemplate[arrCharacter_2];
-                }
-                m_list.Add(_Character_2Temp);
-            }*/
-
-            /* 존 거너
-            for (int i = 0; i < arrCharacter_JOHN.Count; ++i)
-            {
-                LevelGrowTemplate _Character_JOHNTemp = new LevelGrowTemplate(arrCharacter_JOHN[i]);
-                List<LevelGrowTemplate> m_list = null;
-                if (m_dicLevelGrowTemplate.ContainsKey(arrCharacter_JOHN) == false)
-                {
-                    m_list = new List<LevelGrowTemplate>();
-                    m_dicLevelGrowTemplate.Add(arrCharacter_JOHN, m_list);
-                }
-                else
-                {
-                    m_list = m_dicLevelGrowTemplate[arrCharacter_JOHN];
-                }
-                m_list.Add(_Character_JOHNTemp);
-            }*/
-
-            /* 마틴 거너
-            for (int i = 0; i < arrCharacter_MARTHIN.Count; ++i)
-            {
-                LevelGrowTemplate _Character_MARTHINTemp = new LevelGrowTemplate(arrCharacter_MARTHIN[i]);
-                List<LevelGrowTemplate> m_list = null;
-                if (m_dicLevelGrowTemplate.ContainsKey(arrCharacter_MARTHIN) == false)
-                {
-                    m_list = new List<LevelGrowTemplate>();
-                    m_dicLevelGrowTemplate.Add(arrCharacter_MARTHIN, m_list);
-                }
-                else
-                {
-                    m_list = m_dicLevelGrowTemplate[arrCharacter_MARTHIN];
-                }
-                m_list.Add(_Character_MARTHINTemp);
-            }*/
-
-            /*리차드 거너
-            for (int i = 0; i < arrCharacter_RICHARD.Count; ++i)
-            {
-                LevelGrowTemplate _Character_RICHARDTemp = new LevelGrowTemplate(arrCharacter_RICHARD[i]);
-                List<LevelGrowTemplate> m_list = null;
-                if (m_dicLevelGrowTemplate.ContainsKey(arrCharacter_RICHARD) == false)
-                {
-                    m_list = new List<LevelGrowTemplate>();
-                    m_dicLevelGrowTemplate.Add(arrCharacter_RICHARD, m_list);
-                }
-                else
-                {
-                    m_list = m_dicLevelGrowTemplate[arrCharacter_RICHARD];
-                }
-                m_list.Add(_Character_RICHARDTemp);
-            }*/
-
-            /* 로버트 거너
-            for (int i = 0; i < arrCharacter_ROBERT.Count; ++i)
-            {
-                LevelGrowTemplate _Character_ROBERTTemp = new LevelGrowTemplate(arrCharacter_ROBERT[i]);
-                List<LevelGrowTemplate> m_list = null;
-                if (m_dicLevelGrowTemplate.ContainsKey(arrCharacter_ROBERT) == false)
-                {
-                    m_list = new List<LevelGrowTemplate>();
-                    m_dicLevelGrowTemplate.Add(arrCharacter_ROBERT, m_list);
-                }
-                else
-                {
-                    m_list = m_dicLevelGrowTemplate[arrCharacter_ROBERT];
-                }
-                m_list.Add(_Character_ROBERTTemp);
-            }*/
-
-            /* 캐서린 마법사
-            for (int i = 0; i < arrCharacter_CATHERINE.Count; ++i)
-            {
-                 LevelGrowTemplate _Character_CATHERINETemp = new LevelGrowTemplate(arrCharacter_CATHERINE[i]);
-                 List<LevelGrowTemplate> m_list = null;
-                 if (m_dicLevelGrowTemplate.ContainsKey(arrCharacter_CATHERINE) == false)
-                 {
-                     m_list = new List<LevelGrowTemplate>();
-                     m_dicLevelGrowTemplate.Add(arrCharacter_CATHERINE, m_list);
-                 }
-                 else
-                 {
-                     m_list = m_dicLevelGrowTemplate[arrCharacter_CATHERINE];
-                 }
-                 m_list.Add(_Character_CATHERINETemp);
-             }*/
-
         }
     }
     public List<LevelGrowTemplate> GetLevelGrowTemplate(string strName)
